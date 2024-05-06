91f6f3d9ad4834cdaf4afe448e0c562d0c3742cd
mboyr4z <boyr4z.m@gmail.com>
2024-05-06 18:15:01 +0300

    Initial commit

---
+++ Survey/Benimkiler/p.cs
@@ -0,0 +1,5 @@
+public static class p{
+    public static void f(string yazi){
+        Console.WriteLine("[BOYRAZ]"+ yazi);
+    }
+}
---
+++ Survey/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Survey.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
